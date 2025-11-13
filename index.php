0bda5c1d02f0d5fbf614332f5917a5e21fc10460
islombekbekturdiyev10 <bekturdiyevislombek4@gmail.com>
2025-11-13 16:22:42 +0500

    birinchi commit1

--- index.php
+++ index.php
@@ -1,5 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bek </title>
+</head>
+<body>
+
 <?php
-echo "Hello world";
+
+$x = 10;
+$y = 5;
+
+$yigindi = $x + $y;
+$ayirma = $x - $y;
+$kopaytma = $x * $y;
+$bolinma = $x / $y;
+
+
+echo "Yig‘indi = $yigindi <br>";
+echo "Ayirma = $ayirma <br>";
+echo "Ko‘paytma = $kopaytma <br>";
+echo "Bo‘linma = $bolinma <br>";
+?>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
